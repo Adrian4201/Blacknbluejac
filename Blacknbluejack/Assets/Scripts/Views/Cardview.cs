e1d94f0a0f7ae8332d98eb2b0e26051648e718c5
Adrian4201 <cookar6@miamioh.edu>
2025-09-28 23:36:02 -0400

    Added handview annd cardview

---
+++ Blacknbluejack/Assets/Scripts/Views/Cardview.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Cardview : MonoBehaviour
+{
+    [SerializeField] private Sprite Image;
+
+    [SerializeField] private TMP_Text cardNumber;
+
+    [SerializeField] private TMP_Text cardNumberTopr;
+
+    [SerializeField] private TMP_Text cardNumberbttmL;
+
+    [SerializeField] private TMP_Text cardNumberbttmR;
+}
